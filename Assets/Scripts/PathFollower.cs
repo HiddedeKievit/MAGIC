7971ab73084461785a44935e8fea98142145764a
Tibo <tibomelis71004@gmail.com>
2026-02-23 21:02:08 +0100

    Path Follower and Manager.
    + Path Manager
    add to empty object, create more empty objects under that for points, click the parent manager and lock it, then select all points and drag and drop them into the path. you now have a path! (also visible in the editor :D )
    
    + Path Follower.
    Basically add to anything that has the "ICanMove" interface and the path follower will make it follow the path you add in the editor.

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    private ICanMove Mover;
+
+    public PathManager Path;
+
+    public bool IsFollowingPath = true;
+
+    private Vector3 target;
+    private int index = 0;
+
+    void Start()
+    {
+        Mover = GetComponent<ICanMove>();
+        target = Path.checkpoints[index].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFollowingPath)
+            target = Path.checkpoints[index].position;
+
+        float distanceToTarget = Vector3.Distance(transform.position, target);
+
+        // if close to the target
+        if (distanceToTarget < Mover.BaseSize)
+        {
+
+            // slow by dividing the distance by the base size
+            float slowFactor = distanceToTarget / Mover.BaseSize;
+            float adjustedSpeed = Mover.BaseMovementSpeed * slowFactor;
+
+
+            // last checkpoint, stop moving when close enough.
+            if (index + 1 >= Path.checkpoints.Length && distanceToTarget <= 0.1f)
+            {
+                return;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * adjustedSpeed);
+
+            // move slowly towards the last checkpoint
+            MoveToTarget(adjustedSpeed, Mover.BaseRotateSpeed);
+
+
+            if (index + 1 < Path.checkpoints.Length)
+            {
+                index++;
+                return;
+            }
+        } else
+        {
+            MoveToTarget(Mover.BaseMovementSpeed, Mover.BaseRotateSpeed);
+        }
+    }
+
+
+    void MoveToTarget(float speed, float rotateSpeed)
+    {
+
+        // direction from me to target
+        Vector2 direction = ( target - transform.position ).normalized;
+
+        // angle i want to be
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+
+        // rotate towards angle i want to be
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+
+
+        transform.position = transform.position + Time.deltaTime * speed * transform.up;
+    }
+}
