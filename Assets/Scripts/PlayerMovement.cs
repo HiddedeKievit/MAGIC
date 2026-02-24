a9c6f74787a99d3de71619ae344854da23b50afc
Bram <bramarendse@gmail.com>
2026-02-24 12:48:23 +0100

    Sprite changing, sprint & lighting rond player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,64 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 5f;
+    [SerializeField] private bool sprintState;
+    
+    [Header("SpriteChanger")]
+    public SpriteChanger changer;
+
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprintState = false;
     }
     
-    // Update is called once per frame
+    
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            sprintState = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            sprintState = false;
+        }
+
+        if (sprintState == true)
+        {
+            moveSpeed += sprintSpeed;
+        }
+        else 
+        {
+            moveSpeed -= sprintSpeed;
+        }
+
+        if (moveSpeed <= 5)
+        {
+            moveSpeed = 5;
+        }
+
+        if (moveSpeed >= 10)
+        {
+            moveSpeed = 10;
+        }
     }
 
+    //Using the input system to be able to move around
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        changer.SpriteChange();
+        gameObject.GetComponent<SpriteRenderer>().sprite = changer.newSprite;
     }
+
 }
