85238f5b07728197ab010ea1c79f2bd01beafb39
HiddedeKievit <hidde.dekievit@student.hu.nl>
2026-02-26 13:16:13 +0100

    eventjes terug

--- Assets/Scripts/Interfaces/ICanMove.cs
+++ Assets/Scripts/Interfaces/ICanMove.cs
@@ -1,12 +1,6 @@
-using UnityEngine;
 public interface ICanMove
 {
     float BaseMovementSpeed { get; }
     float BaseRotateSpeed { get; }
-    float CurrentSpeed { get; set; }
-    float CurrentRotateSpeed { get; set; }
-
-    Vector3 Target { get; set; }
-    float TargetClearance { get; }
-    bool CanMove { get; set; }
+    float BaseSize { get; }
 }
--- Assets/Scripts/Interfaces/IHasHealth.cs
+++ Assets/Scripts/Interfaces/IHasHealth.cs
@@ -1,4 +1,4 @@
 public interface IHasHealth
 {
-    int Health { get; set; }
+    int Health { get; }
 }
