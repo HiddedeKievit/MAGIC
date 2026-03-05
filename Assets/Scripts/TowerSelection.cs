cac226917ff62eecae74163a3d42fdc2adc879c8
Hidde de Kievit <hidde.dekievit@student.hu.nl>
2026-03-05 11:47:52 +0100

    tower deselect

--- Assets/Scripts/TowerSelection.cs
+++ Assets/Scripts/TowerSelection.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class TowerSelection : MonoBehaviour
 {
+    public static TowerSelection Current;
+
     public bool IsSelected { get; private set; }
 
-    private void OnMouseDown()
+    void OnMouseDown()
     {
-        IsSelected = !IsSelected;
+        Select();
+    }
+
+    public void Select()
+    {
+        if (Current != null && Current != this)
+            Current.Deselect();
+
+        Current = this;
+        IsSelected = true;
     }
 
     public void Deselect()
     {
         IsSelected = false;
+
+        if (Current == this)
+            Current = null;
     }
 }
