915f9f2be4a7c609e75ca580e6f567f4a3a76d85
Bram <bramarendse@gmail.com>
2026-03-09 11:44:35 +0100

    Revert "Merge branch 'main' into Tibo"
    This reverts commit 017a19743d51b4823453c6eb39a3c6a21b06f925, reversing
    changes made to 9269b04f4db83aeb17d3f82cbddfe1cb3277607c.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,9 +4,6 @@ public class Tower : MonoBehaviour
 {
     public TurretData Data { get; private set; }
 
-    [SerializeField] private Transform uiAnchor;
-    public Transform UIAnchor => uiAnchor;
-
     public void Initialize(TurretData data)
     {
         Data = data;
--- Assets/Scripts/TowerSelection.cs
+++ Assets/Scripts/TowerSelection.cs
@@ -2,33 +2,15 @@ using UnityEngine;
 
 public class TowerSelection : MonoBehaviour
 {
-    public static TowerSelection Current;
-
     public bool IsSelected { get; private set; }
 
-    void OnMouseDown()
+    private void OnMouseDown()
     {
-        Select();
-    }
-
-    void Select()
-    {
-        if (Current != null && Current != this)
-            Current.Deselect();
-
-        Current = this;
-        IsSelected = true;
-
-        TowerSelectedUI.Instance.Show(GetComponent<Tower>());
+        IsSelected = !IsSelected;
     }
 
     public void Deselect()
     {
         IsSelected = false;
-
-        if (Current == this)
-            Current = null;
-
-        TowerSelectedUI.Instance.Hide();
     }
 }
