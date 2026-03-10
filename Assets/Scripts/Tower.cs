7794784e96834d0e8a61a782576492027a443491
emmahuizinga08-blip <emmahuizinga08@gmail.com>
2026-03-10 09:04:25 +0100

    Revert "Merge branches 'Emma' and 'main' of https://github.com/HiddedeKievit/MAGIC into Emma"
    This reverts commit ebcc26a4f5f90c20cfc7da6f0742284d12a04c98, reversing
    changes made to df1a7323eed6017c9fe248ceded53b39c21fa255.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,6 +4,9 @@ public class Tower : MonoBehaviour
 {
     public TurretData Data { get; private set; }
 
+    [SerializeField] private Transform uiAnchor;
+    public Transform UIAnchor => uiAnchor;
+
     public void Initialize(TurretData data)
     {
         Data = data;
