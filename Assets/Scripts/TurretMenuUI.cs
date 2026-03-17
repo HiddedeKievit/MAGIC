c32b60783f5172c40b94e8051636beab53990c49
Victor007297 <victor.cardonabedoya@student.hu.nl>
2026-03-17 12:22:05 +0100

    Revert "scaling fix + level pauze"
    This reverts commit 369780ba45217f71f918aa5bbe4823d625f18cdb.

--- Assets/Scripts/TurretMenuUI.cs
+++ Assets/Scripts/TurretMenuUI.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class TurretMenu : MonoBehaviour
 {
@@ -14,9 +14,7 @@ public class TurretMenu : MonoBehaviour
     public void PopulateMenu()
     {
         foreach (Transform child in transform)
-        {
             Destroy(child.gameObject);
-        }
 
         if (BuildManager.Instance == null) return;
 
@@ -25,25 +23,12 @@ public class TurretMenu : MonoBehaviour
 
         for (int i = 0; i < count; i++)
         {
-            GameObject buttonGO = Instantiate(turretButtonPrefab, transform, false);
-
-            RectTransform rt = buttonGO.GetComponent<RectTransform>();
-
-            rt.localScale = Vector3.one;
-            rt.localRotation = Quaternion.identity;
-
-            rt.anchorMin = new Vector2(1f, 0f);
-            rt.anchorMax = new Vector2(1f, 0f);
-            rt.pivot = new Vector2(1f, 0f);
-
-            rt.sizeDelta = new Vector2(181.826f, 185.98f);
-
-      
+            GameObject buttonGO = Instantiate(turretButtonPrefab, transform);
 
             TurretButton button = buttonGO.GetComponent<TurretButton>();
-            button.turretData = turrets[i];
-            button.placementManager = PlacementManager.Instance;
-            button.Setup();
-        }
+            button.turretData = turrets[i];                 // assign correct turret
+            button.placementManager = PlacementManager.Instance; // assign scene object
+            button.Setup();                                // apply icon
         }
     }
+}
